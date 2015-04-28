aee27a8722b5027aad3daabeaa674c5283ec216a
Andrei Scripniciuc <andrei.scripniciuc@gmail.com>
2015-04-28 21:53:35 +0100

    Generate hello world template through maven archetype.

---
+++ mvn-tests/HelloWorld/src/main/java/com/scripni/App.java
@@ -0,0 +1,13 @@
+package com.scripni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
